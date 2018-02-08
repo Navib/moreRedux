0467e16f8393e7907c623fdde617e0fc16b68554
navib <ivan.baena.dev@gmail.com>
2018-02-07 21:22:30 -0500

    working react redux book app

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript'},
-    {title: 'Harry Potter'},
-    {title: 'Dark Tower'},
-    {title: 'Clifford: Big Red Dog'},
+    {title: 'Javascript', pages: 101},
+    {title: 'Harry Potter', pages: 201},
+    {title: 'Dark Tower', pages: 198},
+    {title: 'Clifford: Big Red Dog', pages: 10},
   ]
 }
